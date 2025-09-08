1a4b414a49f503596421a395a499c838db3b22f4
Mo-Ibra <mohamedelgedwy245@gmail.com>
2025-09-08 06:46:20 +0300

    Added Example of ENV file

---
+++ env-example.js
@@ -0,0 +1,16 @@
+/**
+ * This is an example of a .env file
+ * Copy this file to .env and fill in your actual values
+ */
+
+// Your API Key --> Get it from your Paymob dashboard
+PAYMOB_API_KEY="ZXxxxxxxxxxxxxxxx=="
+
+// Your Integration ID
+PAYMOB_INTEGRATION_ID=9999999
+
+// Mobile Wallet (NOT USED)
+// PAYMOB_INTEGRATION_ID_MOBILE_WALLET="9999999"
+
+// Your IFrame ID
+PAYMOB_IFRAME_ID=123123
